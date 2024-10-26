873d88cc295bf4e538b9bd96452cd51e0ef83de9
mrDaimer <damianernst@icloud.com>
2024-10-26 16:15:52 +0200

    Infinite world generation (chunk-based), with camera control (move, zoom + home via space), Debugscreen, Mouse and chunk position

--- szenes/world.gd
+++ szenes/world.gd
@@ -6,7 +6,7 @@ extends Node2D
 @export var worldLayer: TileMapLayer
 @export var skyLayer: TileMapLayer
 
-const source := 0
+const main_source := 0
 const water = Vector2i(0, 0)
 const grass = Vector2i(4, 0)
 const forest = Vector2i(5, 0)
@@ -26,13 +26,13 @@ var chunk_data = {
 }
 
 var curser_tile: Vector2
-var original_tile: Vector2
+var original_tile: Vector2i = grass
 
 func _ready() -> void:
-	set_tile(groundLayer, Vector2i(0, 0), source, grass)
+	set_tile(groundLayer, Vector2i(0, 0), main_source, grass)
 	update_chunks(get_current_chunk())
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var chunk = get_current_chunk()
 	debug_log.text = "Chunk: " + str(chunk) + "\n"
 	if current_chunk != chunk:
@@ -42,8 +42,8 @@ func _process(delta: float) -> void:
 
 func get_current_chunk() -> Vector2:
 	var camera = get_viewport().get_camera_2d().global_position
-	var position = get_tile_of_layer(groundLayer, camera)
-	return Vector2(floor(position.x / CHUNK_SIZE), floor(position.y / CHUNK_SIZE))
+	var tile = get_tile_of_layer(camera)
+	return Vector2(floor(tile.x / CHUNK_SIZE), floor(tile.y / CHUNK_SIZE))
 
 func update_chunks(center: Vector2):
 	for y in range(center.y - RADIUS, center.y + RADIUS + 1):
@@ -62,12 +62,12 @@ func load_chunk(chunk):
 	if chunk_data.has(key):
 		for tile_pos in chunk_data[key]["tiles"].keys():
 			var tile = chunk_data[key]["tiles"][tile_pos]
-			var position = Vector2i(chunk.x * CHUNK_SIZE + tile_pos.x, chunk.y * CHUNK_SIZE + tile_pos.y)
-			set_tile(groundLayer, position, source, tile)
+			var tile_position = Vector2i(chunk.x * CHUNK_SIZE + tile_pos.x, chunk.y * CHUNK_SIZE + tile_pos.y)
+			set_tile(groundLayer, tile_position, main_source, tile)
 	
 	for y in range(CHUNK_SIZE):
 		for x in range(CHUNK_SIZE):
-			set_tile(groundLayer, Vector2i(chunk.x * CHUNK_SIZE + x, chunk.y * CHUNK_SIZE + y), source, water)
+			set_tile(groundLayer, Vector2i(chunk.x * CHUNK_SIZE + x, chunk.y * CHUNK_SIZE + y), main_source, water)
 	chunks[chunk] = true
 
 func unload_chunk(chunk):
@@ -86,7 +86,7 @@ func save_chunk(chunk: Vector2):
 	var only_water = true
 	for y in range(CHUNK_SIZE):
 		for x in range(CHUNK_SIZE):
-			var tile = groundLayer.get_cell_atlas_coords(Vector2i(chunk.x * CHUNK_SIZE + x, chunk.y * CHUNK_SIZE + y))
+			var tile = groundLayer.get_cell_atlas_coords(Vector2(chunk.x * CHUNK_SIZE + x, chunk.y * CHUNK_SIZE + y))
 			if tile != water:
 				chunk_data[key]["tiles"][Vector2i(x, y)] = tile
 				only_water = false
@@ -100,23 +100,23 @@ func set_tile(layer: TileMapLayer, coords: Vector2i, source: int, tile: Vector2)
 	if layer.get_cell_atlas_coords(coords) == Vector2i(-1, -1):
 		layer.set_cell(coords, source, tile)
 
-func get_tile_of_layer(layer: TileMapLayer, position: Vector2):
-	var isometric_width = ((8 * TILE_WIDTH) + (7 * TILE_HEIGHT)) / CHUNK_SIZE
-	var isometric_x = (position.x / isometric_width) + (position.y / TILE_HEIGHT)
-	var isometric_y = (position.y / TILE_HEIGHT) - (position.x / isometric_width)
+func get_tile_of_layer(pos: Vector2):
+	var isometric_width = float((8 * TILE_WIDTH) + (7 * TILE_HEIGHT)) / CHUNK_SIZE
+	var isometric_x = (pos.x / isometric_width) + (pos.y / TILE_HEIGHT)
+	var isometric_y = (pos.y / TILE_HEIGHT) - (pos.x / isometric_width)
 	return Vector2(floor(isometric_x), floor(isometric_y))
 
 func curser():
 	var mouse = get_global_mouse_position()
-	var tile = get_tile_of_layer(groundLayer, mouse)
+	var tile = get_tile_of_layer(mouse)
 	debug_log.text += "Curser: " + str(tile) + "\n"
 	
 	if tile != curser_tile:
 		if original_tile != null:
-			groundLayer.set_cell(curser_tile, source, original_tile)
+			groundLayer.set_cell(curser_tile, main_source, original_tile)
 		
 		original_tile = groundLayer.get_cell_atlas_coords(tile)
-		groundLayer.set_cell(tile, source, forest)
+		groundLayer.set_cell(tile, main_source, forest)
 		
 		curser_tile = tile
 
