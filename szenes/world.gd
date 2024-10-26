5c15b5475b88c029fa209edc13c80cfd5fdd884b
mrDaimer <128384169+mrDaimer@users.noreply.github.com>
2024-10-26 15:03:57 +0200

    Infinite world generation (chunk-based), with camera control (move, zoom + home via space), Debugscreen, Mouse and chunk position

--- szenes/world.gd
+++ szenes/world.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
+@export var debug_log: Label
+
 @export var groundLayer: TileMapLayer
 @export var worldLayer: TileMapLayer
 @export var skyLayer: TileMapLayer
 
 const source := 0
-const block = Vector2i(0, 0)
-const water = Vector2i(2, 0)
+const water = Vector2i(0, 0)
+const grass = Vector2i(4, 0)
+const forest = Vector2i(5, 0)
 
 const TILE_WIDTH := 32
 const TILE_HEIGHT := 16
@@ -22,22 +25,25 @@ var chunk_data = {
 	"tiles": {}
 }
 
+var curser_tile: Vector2
+var original_tile: Vector2
+
 func _ready() -> void:
-	set_tile(groundLayer, Vector2i(0, 0), source, block)
+	set_tile(groundLayer, Vector2i(0, 0), source, grass)
 	update_chunks(get_current_chunk())
 
 func _process(delta: float) -> void:
 	var chunk = get_current_chunk()
+	debug_log.text = "Chunk: " + str(chunk) + "\n"
 	if current_chunk != chunk:
 		current_chunk = chunk
 		update_chunks(chunk)
+	curser()
 
 func get_current_chunk() -> Vector2:
 	var camera = get_viewport().get_camera_2d().global_position
-	var isometric_width = ((8 * TILE_WIDTH) + (7 * TILE_HEIGHT)) / CHUNK_SIZE
-	var isometric_x = (camera.x / isometric_width) + (camera.y / TILE_HEIGHT)
-	var isometric_y = (camera.y / TILE_HEIGHT) - (camera.x / isometric_width)
-	return Vector2(floor(isometric_x / CHUNK_SIZE), floor(isometric_y / CHUNK_SIZE))
+	var position = get_tile_of_layer(groundLayer, camera)
+	return Vector2(floor(position.x / CHUNK_SIZE), floor(position.y / CHUNK_SIZE))
 
 func update_chunks(center: Vector2):
 	for y in range(center.y - RADIUS, center.y + RADIUS + 1):
@@ -82,7 +88,6 @@ func save_chunk(chunk: Vector2):
 		for x in range(CHUNK_SIZE):
 			var tile = groundLayer.get_cell_atlas_coords(Vector2i(chunk.x * CHUNK_SIZE + x, chunk.y * CHUNK_SIZE + y))
 			if tile != water:
-				print("[DEBUG] Tile: " + str(tile))
 				chunk_data[key]["tiles"][Vector2i(x, y)] = tile
 				only_water = false
 	
@@ -94,3 +99,36 @@ func save_chunk(chunk: Vector2):
 func set_tile(layer: TileMapLayer, coords: Vector2i, source: int, tile: Vector2):
 	if layer.get_cell_atlas_coords(coords) == Vector2i(-1, -1):
 		layer.set_cell(coords, source, tile)
+
+func get_tile_of_layer(layer: TileMapLayer, position: Vector2):
+	var isometric_width = ((8 * TILE_WIDTH) + (7 * TILE_HEIGHT)) / CHUNK_SIZE
+	var isometric_x = (position.x / isometric_width) + (position.y / TILE_HEIGHT)
+	var isometric_y = (position.y / TILE_HEIGHT) - (position.x / isometric_width)
+	return Vector2(floor(isometric_x), floor(isometric_y))
+
+func curser():
+	var mouse = get_global_mouse_position()
+	var tile = get_tile_of_layer(groundLayer, mouse)
+	debug_log.text += "Curser: " + str(tile) + "\n"
+	
+	if tile != curser_tile:
+		if original_tile != null:
+			groundLayer.set_cell(curser_tile, source, original_tile)
+		
+		original_tile = groundLayer.get_cell_atlas_coords(tile)
+		groundLayer.set_cell(tile, source, forest)
+		
+		curser_tile = tile
+
+
+#if tile_position != highlighted_tile_pos:
+		## Vorheriges Tile zurücksetzen, wenn es existiert
+		#if highlighted_tile_pos != null:
+			#tile_map_layer.set_cell(highlighted_tile_pos.x, highlighted_tile_pos.y, original_tile_id)
+#
+		## Neues ursprüngliches Tile speichern und Hover-Tile setzen
+		#original_tile_id = tile_map_layer.get_cell(tile_position.x, tile_position.y)
+		#tile_map_layer.set_cell(tile_position.x, tile_position.y, hover_tile_id)
+#
+		## Aktualisiere die hervorgehobene Position
+		#highlighted_tile_pos = tile_position
