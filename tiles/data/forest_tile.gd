fc0ca0b0d6ea78dbdee17374772fb4e2930f682e
Lukas <lukas_nickel@gmx.de>
2024-11-14 22:48:38 +0100

    :sparkles: added decoration system

--- tiles/data/forest_tile.gd
+++ tiles/data/forest_tile.gd
@@ -1,10 +1,23 @@
 class_name ForestTile
 extends Tile
 
+var decorations = {
+	"Forest_1": {
+		"source_id": 2,
+		"atlas_coords": Vector2i(0, 0),
+		"probability": 0.5
+	},
+	"Forest_2": {
+		"source_id": 2,
+		"atlas_coords": Vector2i(1, 0),
+		"probability": 0.5
+	}
+}
+
 func _init(global_coords: Vector2i) -> void:
 	var name = "Forest"
 	var source_id = 0
 	var atlas_coords = Vector2i(5, 0)
 	var replaceable = false
-	var decoration = TreeDecoration.new()
+	var decoration = null
 	super(name, global_coords, source_id, atlas_coords, replaceable, decoration)
