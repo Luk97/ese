4a59e7481296a67e723364ef964a4915d25fbedc
VectorVermillion <Rinne.Tobias@gmx.de>
2024-11-26 12:48:16 +0100

    Added Code for woodlable

--- interface/wood_counter_lable.gd
+++ interface/wood_counter_lable.gd
@@ -1,6 +1,6 @@
-extends NinePatchRect
+extends Label
 
-@onready var wood_counter_label: Label = %WoodCounterLabel
+@onready var wood_counter_label: Label = %Wood_Counter_Label
 
 func set_wood_count(wood_count: int) -> void:
 	wood_counter_label.text = str("Wood:", wood_count)
