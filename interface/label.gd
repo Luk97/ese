a592f394bd724a27af11abfcdfea887d04cab423
VectorVermillion <Rinne.Tobias@gmx.de>
2024-11-26 12:27:35 +0100

    Lable Wood added but hidden

--- interface/label.gd
+++ interface/label.gd
@@ -1,6 +1,7 @@
-extends NinePatchRect
+extends Label
+
 
 @onready var wood_counter_label: Label = %WoodCounterLabel
 
 func set_wood_count(wood_count: int) -> void:
-	wood_counter_label.text = str(wood_count)
+	wood_counter_label.text = str("Wood: ", wood_count)
