ab077ab4414b50689df2f1a89624998652a1c7ba
VectorVermillion <Rinne.Tobias@gmx.de>
2024-12-03 15:06:43 +0100

    Update main_menu.gd

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -1,6 +1,6 @@
 extends Control
-var thisFont = preload("res://assets/PixelArt.ttf")
-var fontScale=70
+#var thisFont = preload("res://assets/PixelArt.ttf")
+#var fontScale=70
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
