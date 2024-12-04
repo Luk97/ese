c38d436f1d005d115c6f16364213fb7bd78ca568
VectorVermillion <Rinne.Tobias@gmx.de>
2024-12-04 23:15:17 +0100

    scaling rework

--- main_menu/label.gd
+++ main_menu/label.gd
@@ -1,6 +1,6 @@
 extends Label
-var fontScale=10
-
+var fontScaleX=10
+var fontScaleY=5
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	
@@ -9,6 +9,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var thisFontSize = get_window().size.x / fontScale + get_window().size.y / fontScale
+	var thisFontSize = min(get_window().size.x / fontScaleX, get_window().size.y / fontScaleY)
 	add_theme_font_size_override("font_size",thisFontSize)
 	pass
