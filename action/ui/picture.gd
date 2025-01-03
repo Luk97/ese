68a74ce9a99038633e5e8dda56fd2d1c2200e0ea
VectorVermillion <Rinne.Tobias@gmx.de>
2025-01-03 17:46:06 +0100

    sizing issue still need to be solved

--- action/ui/picture.gd
+++ action/ui/picture.gd
@@ -4,11 +4,13 @@ signal picture_clicked
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#var sizing=size.y
+	#size=Vector2(sizing,sizing)
 	pass
 
 
