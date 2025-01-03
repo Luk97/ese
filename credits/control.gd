382d3caf9ec77d474230a4d9d93306c0b66bee4c
VectorVermillion <Rinne.Tobias@gmx.de>
2025-01-03 18:40:40 +0100

    Some Changes

--- credits/control.gd
+++ credits/control.gd
@@ -1,10 +1,16 @@
 extends Control
 
-var labelFontScaleX=10
-var labelFontScaleY=5
+
 @onready var ui: Label = %UI
 @onready var graphics: Label = %Graphics
 @onready var gamelogic: Label = %Gamelogic
+@onready var beendenButton: Button = %BeendenButton
+
+
+var labelFontScaleX=10
+var labelFontScaleY=5
+var ButtonFontScaleX=30
+var ButtonFontScaleY=15
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,3 +23,5 @@ func _process(delta: float) -> void:
 	ui.add_theme_font_size_override("font_size",thisLabelFontSize)
 	graphics.add_theme_font_size_override("font_size",thisLabelFontSize)
 	gamelogic.add_theme_font_size_override("font_size",thisLabelFontSize)
+	var thisButtonFontSize=min(get_window().size.x / ButtonFontScaleX, get_window().size.y / ButtonFontScaleY)
+	beendenButton.add_theme_font_size_override("font_size",thisButtonFontSize)
