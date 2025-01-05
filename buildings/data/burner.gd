3fa18ac7c5be513d07614d625cc15f56e9e6838c
mrDaimer <128384169+mrDaimer@users.noreply.github.com>
2025-01-05 18:33:10 +0100

    TileSet update

--- buildings/data/burner.gd
+++ buildings/data/burner.gd
@@ -6,7 +6,7 @@ func _init() -> void:
 	self.cost = { Types.ResourceType.WOOD: 6 }
 	self.view_radius = 2
 	self. source_id = 0
-	self. atlas_coords = Vector2i(4, 9)
+	self. atlas_coords = Vector2i(4, 7)
 
 func is_valid_placement_tile(type: Types.TileType) -> bool:
 	return type in [Types.TileType.MOUNTAIN]
