5f2c74026cbd6a68501121b6256ce050f6050d29
VectorVermillion <Rinne.Tobias@gmx.de>
2025-01-08 15:36:03 +0100

    Autoscaling works now. Could still be improved

--- game_over/game_over_screen.gd
+++ game_over/game_over_screen.gd
@@ -6,6 +6,8 @@ extends CanvasLayer
 @onready var newGameButton: Button=%NewGame
 @onready var menuButton: Button=%Menu
 
+var titleFontScale=0.5
+var buttonFontScale=0.1
 var buttonScaleXY=3
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,4 +16,14 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var windowXY=get_window().size.x /get_window().size.y
+	var thisTitleFontSize = min(get_window().size.x * titleFontScale/windowXY*0.33, get_window().size.y * titleFontScale)
+	gameOverLabel.add_theme_font_size_override("font_size",thisTitleFontSize)
+	
+	var thisButtonsFontSize = min(get_window().size.x * buttonFontScale/windowXY, get_window().size.y * buttonFontScale)
+	var buttonx=max(newGameButton.size.x,menuButton.size.x)
+	var buttony=max(newGameButton.size.y,menuButton.size.y)
+	newGameButton.size=Vector2(buttonx,buttony)
+	menuButton.size=Vector2(buttonx,buttony)
+	newGameButton.add_theme_font_size_override("font_size",thisButtonsFontSize)
+	menuButton.add_theme_font_size_override("font_size",thisButtonsFontSize)
