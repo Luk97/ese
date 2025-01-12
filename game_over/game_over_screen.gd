0b1e34fdce08acc39c31950c229cc31cf8fc2927
VectorVermillion <Rinne.Tobias@gmx.de>
2025-01-12 18:11:57 +0100

    Added Scaling and alignment to GameoverMessage

--- game_over/game_over_screen.gd
+++ game_over/game_over_screen.gd
@@ -20,8 +20,9 @@ func _process(delta: float) -> void:
 	var buttonx=max(newGameButton.size.x,menuButton.size.x)
 	var buttony=max(newGameButton.size.y,menuButton.size.y)
 	newGameButton.size=Vector2(buttonx,buttony)
+	
 	menuButton.size=Vector2(buttonx,buttony)
 	newGameButton.add_theme_font_size_override("font_size",thisButtonsFontSize)
 	menuButton.add_theme_font_size_override("font_size",thisButtonsFontSize)
-	
+	gameOverMessage.add_theme_font_size_override("font_size",thisButtonsFontSize/1.5)
 	gameOverMessage.text = str("You survived ", GameManager.rounds, " rounds. Congratulations!")
