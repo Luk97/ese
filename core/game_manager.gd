3b62e6d51aac0da9a318cddef3486791ee8ec7ba
Lukas <lukas_nickel@gmx.de>
2025-01-12 13:20:54 +0100

    :sparkles: added feature to increase progress bar

--- core/game_manager.gd
+++ core/game_manager.gd
@@ -6,17 +6,12 @@ const START_WOOD = 20
 const START_STONE = 10
 const START_FOOD = 10
 
-const START_WARMTH_NEED = 10
-const START_FOOD_NEED = 10
-
 # Global core variables
 var rng = RandomNumberGenerator.new()
 var show_world = false
 
 # Game stats
 @export var rounds: int = START_ROUNDS
-@export var warmth_decrease = 1
-@export var food_decrease = 1
 
 # Global signals
 signal round_updated(int)
@@ -26,6 +21,7 @@ func initialize_game() -> void:
 	rng = RandomNumberGenerator.new()
 	TileManager.reset_tiles()
 	CameraController.setup()
+	ResourceManager.initialize_resources()
 	
 	if not show_world:
 		emit_signal("round_updated", rounds)
@@ -34,3 +30,9 @@ func initialize_game() -> void:
 func update_rounds() -> void:
 	rounds += 1
 	emit_signal("round_updated", rounds)
+
+func get_warmth_increase_cost() -> int:
+	return self.rounds / 10 + 1
+
+func get_food_increase_cost() -> int:
+	return self.rounds / 10 + 1
